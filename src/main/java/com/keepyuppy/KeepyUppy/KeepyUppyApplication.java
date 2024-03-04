7bae16703a8f43940044c844a3c23fd2f09d1790
cmong0516 <cmong0516@gmail.com>
2024-03-04 11:17:30 +0900

    Project Init

---
+++ src/main/java/com/keepyuppy/KeepyUppy/KeepyUppyApplication.java
@@ -0,0 +1,13 @@
+package com.keepyuppy.KeepyUppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepyUppyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepyUppyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keepyuppy/KeepyUppy/KeepyUppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepyuppy.KeepyUppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepyUppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
