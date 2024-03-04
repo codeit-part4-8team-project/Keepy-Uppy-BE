d507326b81ae65c0f4020d887048f44b9ee7a8fc
cherin kim <76779953+mawakeb@users.noreply.github.com>
2024-03-04 12:42:16 +0900

    refactor[User] : Make Users extend BaseTimeEntity and getters protected

--- src/main/java/com/keepyuppy/KeepyUppy/user/domain/entity/Users.java
+++ src/main/java/com/keepyuppy/KeepyUppy/user/domain/entity/Users.java
@@ -1,20 +1,18 @@
 package com.keepyuppy.KeepyUppy.user.domain.entity;
 
+import com.keepyuppy.KeepyUppy.global.domain.BaseTimeEntity;
 import com.keepyuppy.KeepyUppy.user.domain.enums.Provider;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter
+@Getter(AccessLevel.PROTECTED)
 @NoArgsConstructor
 @ToString
-public class Users {
+public class Users extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
