29eabe69aaa2e9d3eabdbea9ad3b04fb6f7dbab3
cmong0516 <cmong0516@gmail.com>
2024-03-17 05:05:56 +0900

    feat[Schedule]: schedule CRUD

--- src/main/java/com/keepyuppy/KeepyUppy/schedule/communication/response/ScheduleResponse.java
+++ src/main/java/com/keepyuppy/KeepyUppy/schedule/communication/response/ScheduleResponse.java
@@ -1,36 +1,13 @@
-//package com.keepyuppy.KeepyUppy.schedule.communication.response;
-//
-//
-//import com.keepyuppy.KeepyUppy.member.communication.response.MemberResponse;
-//import com.keepyuppy.KeepyUppy.schedule.domain.entity.Schedule;
-//import io.swagger.v3.oas.annotations.media.Schema;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//
-//import java.time.LocalDateTime;
-//
-//@Data
-//@Schema(name = "스케줄 정보 응답")
-//@AllArgsConstructor
-//public class ScheduleResponse {
-//
-//    private Long id;
-//    private String title;
-//    private MemberResponse author;
-//    private String content;
-//    private LocalDateTime startDateTime;
-//    private LocalDateTime endDateTime;
-//
-//    public static ScheduleResponse of(Schedule schedule){
-//        return new ScheduleResponse(
-//                schedule.getId(),
-//                schedule.getTitle(),
-//                MemberResponse.of(schedule.getAuthor()),
-//                schedule.getContent(),
-//                schedule.getStartDateTime(),
-//                schedule.getEndDateTime()
-//        );
-//    }
-//
-//
-//}
+package com.keepyuppy.KeepyUppy.schedule.communication.response;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public abstract class ScheduleResponse {
+    private String title;
+    private String content;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+}
