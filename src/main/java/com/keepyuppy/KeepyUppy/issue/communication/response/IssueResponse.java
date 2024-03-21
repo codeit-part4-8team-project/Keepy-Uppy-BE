de733de63929f54f9ae57ee67f9a2ac5c1286278
cmong0516 <cmong0516@gmail.com>
2024-03-22 03:23:51 +0900

    refactor[User]: remove unuseMethod, duplicate Class

--- src/main/java/com/keepyuppy/KeepyUppy/issue/communication/response/IssueResponse.java
+++ src/main/java/com/keepyuppy/KeepyUppy/issue/communication/response/IssueResponse.java
@@ -1,7 +1,7 @@
 package com.keepyuppy.KeepyUppy.issue.communication.response;
 
 import com.keepyuppy.KeepyUppy.issue.domain.entity.Issue;
-import com.keepyuppy.KeepyUppy.team.communication.response.TeamInContentResponse;
+import com.keepyuppy.KeepyUppy.team.communication.response.TeamWithoutMemberResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,11 +9,11 @@ import lombok.Data;
 @Schema(name = "팀 정보 포함한 이슈 정보 응답")
 public class IssueResponse extends TeamIssueResponse {
 
-    private TeamInContentResponse team;
+    private TeamWithoutMemberResponse team;
 
     public IssueResponse(Issue issue) {
         super(issue);
-        this.team = TeamInContentResponse.of(issue.getTeam());
+        this.team = TeamWithoutMemberResponse.of(issue.getTeam());
     }
 
     public static IssueResponse of(Issue issue){
