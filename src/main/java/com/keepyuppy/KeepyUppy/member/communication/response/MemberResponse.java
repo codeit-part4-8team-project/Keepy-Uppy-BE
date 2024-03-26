eeb1769b8418743c18d2735181b12a773c550826
cmong0516 <cmong0516@gmail.com>
2024-03-26 15:32:23 +0900

    refactor[MemberJoinDate]: LocalDateTime -> LocalDate

--- src/main/java/com/keepyuppy/KeepyUppy/member/communication/response/MemberResponse.java
+++ src/main/java/com/keepyuppy/KeepyUppy/member/communication/response/MemberResponse.java
@@ -4,7 +4,7 @@ import com.keepyuppy.KeepyUppy.member.domain.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @Schema(name = "멤버 정보 응답")
@@ -14,7 +14,7 @@ public class MemberResponse {
     private String role;
     private String grade;
     private String username;
-    private LocalDateTime createdDate;
+    private LocalDate createdDate;
 
     public MemberResponse(Member member) {
         this.name = member.getUser() == null ? null : member.getUser().getName();
@@ -22,7 +22,7 @@ public class MemberResponse {
         this.role = member.getRole() == null ? null : member.getRole().name();
         this.grade = member.getGrade().name();
         this.username = member.getUser() == null ? null : member.getUser().getUsername();
-        this.createdDate = member.getCreatedDate();
+        this.createdDate = LocalDate.of(member.getCreatedDate().getYear(), member.getCreatedDate().getMonth(), member.getCreatedDate().getDayOfMonth());
     }
 
     public static MemberResponse of(Member member) {
