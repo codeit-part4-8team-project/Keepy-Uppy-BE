d961af0809f913d4ebbe544f3d738e96dc489564
cmong0516 <cmong0516@gmail.com>
2024-03-26 13:19:35 +0900

    refactor[Member]: add createdDate , setJoinTeamDateTime

--- src/main/java/com/keepyuppy/KeepyUppy/member/communication/response/MemberResponse.java
+++ src/main/java/com/keepyuppy/KeepyUppy/member/communication/response/MemberResponse.java
@@ -4,6 +4,8 @@ import com.keepyuppy.KeepyUppy.member.domain.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Schema(name = "멤버 정보 응답")
 public class MemberResponse {
@@ -12,6 +14,7 @@ public class MemberResponse {
     private String role;
     private String grade;
     private String username;
+    private LocalDateTime createdDate;
 
     public MemberResponse(Member member) {
         this.name = member.getUser() == null ? null : member.getUser().getName();
@@ -19,10 +22,10 @@ public class MemberResponse {
         this.role = member.getRole() == null ? null : member.getRole().name();
         this.grade = member.getGrade().name();
         this.username = member.getUser() == null ? null : member.getUser().getUsername();
+        this.createdDate = member.getCreatedDate();
     }
 
     public static MemberResponse of(Member member) {
         return new MemberResponse(member);
     }
 }
-
