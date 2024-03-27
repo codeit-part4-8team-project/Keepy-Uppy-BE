d7cc56c38a610f69e8448fcaf5ca45647a104e00
cmong0516 <74821906+cmong0516@users.noreply.github.com>
2024-03-27 11:14:06 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
   echo "> kill  $CURRENT_PID"
-  sudo kill $CURRENT_PID
+  sudo kill -9 $CURRENT_PID
   sleep 5
 fi
 
